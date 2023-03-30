156b8abd55f7f7e182f14ae7d5a6d6ea5ff5d4d1
monyarm <monyarm@gmail.com>
2023-03-30 15:23:34 +0300

    fix typo

--- modules/distcc.nix
+++ modules/distcc.nix
@@ -5,5 +5,5 @@
   services.distccd.openFirewall = true;
   services.distccd.stats.enable = true;
   services.distccd.allowedClients = ["127.0.0.1" "192.168.1.0"];
-  environment.sessionVariables.DISTCC_HOSTS = "raych virtual acer-vx15-nixos mcl-nixos-desktop01  mcl-nixos-desktop05 zlx-flow-x13 zlx-nixos-desktop zlx-nixos-desktop2";
+  environment.sessionVariables.DISTCC_HOSTS = "raycho virtual acer-vx15-nixos mcl-nixos-desktop01  mcl-nixos-desktop05 zlx-flow-x13 zlx-nixos-desktop zlx-nixos-desktop2";
 }
