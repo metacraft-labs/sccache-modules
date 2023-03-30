0167cb39ab567d3d62e4f9fbbbcdd1b2de0522c8
monyarm <monyarm@gmail.com>
2023-03-30 15:36:10 +0300

    network mask

--- modules/distcc.nix
+++ modules/distcc.nix
@@ -4,6 +4,6 @@
   services.distccd.zeroconf = true;
   services.distccd.openFirewall = true;
   services.distccd.stats.enable = true;
-  services.distccd.allowedClients = ["127.0.0.1" "192.168.1.0"];
+  services.distccd.allowedClients = ["127.0.0.1" "192.168.1.0/24"];
   environment.sessionVariables.DISTCC_HOSTS = "raycho virtual acer-vx15-nixos mcl-nixos-desktop01  mcl-nixos-desktop05 zlx-flow-x13 zlx-nixos-desktop zlx-nixos-desktop2";
 }
