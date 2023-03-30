15a922a073c3267051950504c90bf7b1783aa8e5
monyarm <monyarm@gmail.com>
2023-03-30 13:30:17 +0300

    disable scheduler

--- modules/icecream.nix
+++ modules/icecream.nix
@@ -3,7 +3,7 @@
   services.icecream.daemon.enable = true;
   services.icecream.daemon.openFirewall = true;
   services.icecream.daemon.openBroadcast = true;
-  services.icecream.scheduler.enable = true;
+  # services.icecream.scheduler.enable = true;
   services.icecream.scheduler.openFirewall = true;
   # services.icecream.scheduler.openBroadcast = true;
 }
